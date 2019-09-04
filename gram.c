6540af9d0359e0489c6b80801b6b2733ad6a8a1d
YUJI takahashi <jit.y.31@gmail.com>
2019-09-04 21:27:21 +0900

    welcome message

--- gram.c
+++ gram.c
@@ -10,6 +10,7 @@
 
 /* defines */
 
+#define KILO_VERSION "0.0.1"
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 /* data */
@@ -128,7 +129,24 @@ void abFree(struct abuf *ab) {
 void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    abAppend(ab, "~", 1);
+    if (y == E.screenrows / 3) {
+      char welcome[80];
+      int welcomelen = snprintf(welcome, sizeof(welcome), "Kilo editor -- version %s", KILO_VERSION);
+
+      if (welcomelen > E.screenrows) welcomelen = E.screenrows;
+      int padding = (E.screencols - welcomelen) / 2;
+
+      if (padding) {
+        abAppend(ab, "~", 1);
+        padding--;
+      }
+
+      while (padding--) abAppend(ab, " ", 1);
+      abAppend(ab, welcome, welcomelen);
+
+    } else {
+      abAppend(ab, "~", 1);
+    }
 
     abAppend(ab, "\x1b[K", 3);
     if (y < E.screenrows - 1) {
